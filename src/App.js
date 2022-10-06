6d5561a2eb922a8a01272122c014bdedc12c88d1
fherrerao <fherrera0206@gmail.com>
2022-10-06 12:33:21 -0500

    Add left and right components as a child of SplitScreen  component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import SplitScreen from "./SplitScreen";
+
+const LeftHandComponent = ({name}) => (
+  <h1 style={{backgroundColor: 'green'}}>{name}</h1>
+);
+
+const RightHandComponent = ({message}) => (
+  <p style={{backgroundColor: 'red'}}>{message}</p>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Fernando"/>
+      <RightHandComponent message="Hello"/>
+    </SplitScreen>    
+  )
 }
 
 export default App;
