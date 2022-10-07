845612728ed3033fa73a2d5a9b88e90870ac7fac
fherrerao <fherrera0206@gmail.com>
2022-10-07 08:29:24 -0500

    Added components for people and products

---
+++ src/people/LargePersonListItem.jsx
@@ -0,0 +1,16 @@
+const LargePersonListItem = ({ person }) => {
+  const {name, age, hairColor, hobbies} = person;
+  return(
+    <>
+      <h3>{name}</h3>
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
+      <h3>Hobbies:</h3>
+      <ul>
+        {hobbies.map(element => <li key={element}>{element}</li>)}
+      </ul>      
+    </>
+  );
+}
+
+export default LargePersonListItem;
